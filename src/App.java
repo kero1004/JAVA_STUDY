4426c2b6c298b8e5446d1b49ac876e1b8ffd9100
kero <jdy410@naver.com>
2021-05-21 00:44:09 +0900

    Spliterator 예제

--- src/App.java
+++ src/App.java
@@ -6,11 +6,18 @@ public class App {
 
     public static void main(String[] args) {
 
-        List<String> name = Arrays.asList("whitesheet", "dy", "yc", "goorm");
+        List<String> name = Arrays.asList("whitesheet", "kero", "dd", "goorm");
 
-        name.forEach(System.out::println);  //메서드 레퍼런스 - 리턴값이 없는 경우
-        System.out.println("============");
-        name.forEach(s -> System.out.println(s));
+//        name.forEach(System.out::println);  //메서드 레퍼런스 - 리턴값이 없는 경우
+//        System.out.println("============");
+//        name.forEach(s -> System.out.println(s));
+
+
+        Spliterator<String> spliterator = name.spliterator();
+        Spliterator<String> spliterator1 = spliterator.trySplit();
+        while(spliterator.tryAdvance(System.out::println));
+        System.out.println("==========");
+        while(spliterator1.tryAdvance(System.out::println));
 
     }
 }
